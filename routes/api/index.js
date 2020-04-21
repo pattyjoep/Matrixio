705e7e3ffce24e828193213508053ccf5f666513
Matt Dambra <mpdambra@gmail.com>
2020-04-21 19:04:35 -0400

    Adding comment to routes file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const matrixRoutes = require("./matrices");
 
-//
+//use the matrix routes, just a skeleton.
 router.use("/matrices", matrixRoutes);
 
 module.exports = router;
