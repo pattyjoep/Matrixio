f76726b544ad9cdf730ecff7bfddb8503e76bd0f
Patrick Dunn <pmdunn78@gmail.com>
2020-04-21 22:02:13 -0400

    mixed matrix schema

--- models/Matrix.js
+++ models/Matrix.js
@@ -1,16 +1,18 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const MatrixSchema = new Schema({
-  rows: {
-    type: Number,
-    trim: true
-  },
 
-  columns: {
-    type: Number,
-    trim: true
+
+
+
+const MatrixSchema = new Schema({
+  
+
+  matrix: {
+    type: Mixed,
   }
+  
+  
 });
 
 const Matrix = mongoose.model("Matrix", MatrixSchema);
