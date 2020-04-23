4c3918e7dab9463e82f633817cb2e9776fddecd8
Matt Dambra <mpdambra@gmail.com>
2020-04-23 14:13:16 -0400

    Adding matrixController

---
+++ controllers/matrixController.js
@@ -0,0 +1,36 @@
+const db = require("../models");
+
+//Methods for matrixRoutes.js
+module.exports = {
+  findAll: function(req, res) {
+    db.Matrix.find(req.query)
+      .sort({ _id: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  findById: function(req, res) {
+    db.Matrix.findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Matrix.create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  /**
+   * TODO:
+   * Require a little additional coding to update only the square in question.
+  **/
+  update: function(req, res) {
+    db.Matrix.findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Matrix.findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
+  }
+};
