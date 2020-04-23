1b02367e518065a8f7a4bf71f1320becff6bf0d7
Matt Dambra <mpdambra@gmail.com>
2020-04-23 13:22:39 -0400

    removed extra white space

--- models/Matrix.js
+++ models/Matrix.js
@@ -1,18 +1,11 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
-
-
-
 const MatrixSchema = new Schema({
   
-
   matrix: {
     type: Mixed,
   }
-  
-  
 });
 
 const Matrix = mongoose.model("Matrix", MatrixSchema);
