aa00bff658987749e02df976e7925517da0d28cf
Trick <petropatrickj@gmail.com>
2020-04-22 21:55:28 -0400

    adding login page

--- client/src/pages/Login/index.js
+++ client/src/pages/Login/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./style.css";
 import Container from "../../components/Container";
+import Login from "../../components/Login";
 
-function Login() {
-  return <Container></Container>;
+function LoginPage() {
+  return (
+    <Login></Login>
+  )
 }
 
-export default Login;
+export default LoginPage;
