df478d24a7ff5ecef948ffa8114ccf8d84c828b0
shelby palumbo <shelbypalumbo94@gmail.com>
2020-04-24 19:08:04 -0400

    Removing upload image files

--- client/src/pages/Students/index.js
+++ client/src/pages/Students/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./style.css";
 import Container from "../../components/Container";
 import StudentList from "../../components/StudentList";
-import Jumbotron from "../../components/Jumbotron";
 import NavBar from "../../components/NavBar";
 import Row from "../../components/Row";
 
@@ -10,7 +9,6 @@ function Students() {
   console.log("Students page");
   return (
     <div>
-      <Jumbotron />
       <NavBar />
       <Container>
         <Row>
