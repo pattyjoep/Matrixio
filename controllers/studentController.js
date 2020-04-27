654a3c7a75c346b8f3257a5a49cecd713c470fd8
Patrick Dunn <pmdunn78@gmail.com>
2020-04-27 18:58:48 -0400

    create student route

--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,33 +1,39 @@
 const db = require("../models");
 
+
+
 //Methods for studentRoutes.js
 module.exports = {
-  findAll: function(req, res) {
-    db.Student.find(req.query)
-      .sort({ lastName: -1 })
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
+  
+
+
+  
+
+
+  
+
+  create: ({ body }, res) => {
+    db.Student.create(body)
+    .then(( { _id }) => db.Teacher.findByIdAndUpdate(
+      {},
+      { 
+        $push: {
+          students: _id
+        }
+      },
+      {
+        new: true
+      } 
+      ))
+      .then(dbTeacher => {
+        res.json(dbTeacher);
+      })
+      .catch(err => {
+        res.json(err);
+      });
   },
-  findById: function(req, res) {
-    db.Student.findById(req.params.id)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  create: function(req, res) {
-    db.Student.create(req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  //not sure what this would be used for other than changing student name or something. But this existed in teacher so figured I would add it as well.
-  update: function(req, res) {
-    db.Student.findOneAndUpdate({ _id: req.params.id }, req.body)
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  },
-  remove: function(req, res) {
-    db.Student.findById({ _id: req.params.id })
-      .then(dbModel => dbModel.remove())
-      .then(dbModel => res.json(dbModel))
-      .catch(err => res.status(422).json(err));
-  }
+
+
+
+  
 };
--- routes/api/student.js
+++ routes/api/student.js
@@ -1,22 +1,17 @@
 const router = require("express").Router();
 const studentController = require("../../controllers/studentController");
 
-//Matches with "/api/student/register"
-//Might want to change to "addnew" or soemthing that reflects adding a new student to each teachers' list of students
-router
-  .route("/addnew")
-  .post(studentController.create);
+
 
 //Matches with "/api/student"
 router
   .route("/")
-  .get(studentController.findAll)
+  
+  .post(studentController.create)
+
+
   
 //Matches with "/api/student/:id"
-router
-  .route("/student/:id")
-  .get(studentController.findById)
-  .put(studentController.update)
-  .delete(studentController.remove);
+
 
 module.exports = router;
