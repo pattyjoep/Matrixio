000c942fec6fe71b2ce7212aed41137733486024
Matt Dambra <mpdambra@gmail.com>
2020-04-27 19:38:03 -0400

    more whitespace bud

--- routes/api/student.js
+++ routes/api/student.js
@@ -1,17 +1,9 @@
 const router = require("express").Router();
 const studentController = require("../../controllers/studentController");
 
-
-
 //Matches with "/api/student"
 router
   .route("/")
-  
   .post(studentController.create)
-
-
-  
-//Matches with "/api/student/:id"
-
 
 module.exports = router;
