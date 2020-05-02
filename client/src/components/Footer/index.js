279c73e0b40fdec3ef39905a98c532cb283c9889
shelby palumbo <shelbypalumbo94@gmail.com>
2020-05-02 00:33:21 -0400

    trying gradient styling on jumbotron and navbar

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./style.css";
+import logo from "./MatrixioLogoSolo.png";
 
 function Footer() {
   return (
     <footer>
       <div className="footer">
+        <img className="footer-logo" src={logo} />
+        &nbsp;&nbsp;
         <span>
           &#169; Matrixio 2020 &nbsp; <i className="fab fa-facebook"></i> &nbsp;{" "}
           <i className="fab fa-instagram"></i>&nbsp; &nbsp; (860)-867-5309
-        </span>
+        </span>{" "}
+        &nbsp;&nbsp;
+        <img className="footer-logo" src={logo} />
       </div>
     </footer>
   );
