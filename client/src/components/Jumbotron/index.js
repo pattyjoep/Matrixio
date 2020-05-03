263c75c995a26e39bd5c4be1cfa154cf28fc9b8e
shelby palumbo <shelbypalumbo94@gmail.com>
2020-05-02 20:54:52 -0400

    trying other background

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./style.css";
+import logo from "./logo.png";
 
 function Jumbotron() {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="jumbodisplay">
+        <img src={logo} className="logo" alt="MatrixioLogo" />
         <img
           src="img/MatrixioLogoFull.png"
           alt="MatrixioLogo"
