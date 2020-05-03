be3dc465a14335c0f85050d71249dc6010ea3d29
shelby palumbo <shelbypalumbo94@gmail.com>
2020-05-02 21:29:16 -0400

    Removing logo animation from jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,12 +1,10 @@
 import React from "react";
 import "./style.css";
-import logo from "./logo.png";
 
 function Jumbotron() {
   return (
     <div className="jumbotron jumbotron-fluid">
       <div className="jumbodisplay">
-        <img src={logo} className="logo" alt="MatrixioLogo" />
         <img
           src="img/MatrixioLogoFull.png"
           alt="MatrixioLogo"
