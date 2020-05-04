ba16eff00af8a3da7008454637bf7cd6c2a58693
Matt Dambra <mpdambra@gmail.com>
2020-05-04 12:29:54 -0400

    adding date objects to collection

--- models/Matrix.js
+++ models/Matrix.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema;
 
 /**
  * * * * * Matrix Schema * * * * *
- * 
+ * Mongoose model for the matrix collection in mongoDB.
+ * StudentID: A reference to the student who this matrix refers to.
+ * dateCreated: The datetime that this matrix was created of type Date.now.
+ * lastUpdated: The last datetime that this matrix was updated.
  */
 const MatrixSchema = new Schema({
 
@@ -11,9 +14,26 @@ const MatrixSchema = new Schema({
 
   StudentID: {
     type: String
+  },
+
+  dateCreated: {
+    tpye: Date,
+    default: Date.now
+  },
+
+  lastUpdated: {
+    type: Date
   }
 
 }, { collection: "matrices" });
+
+MatrixSchema.methods.setLastUpdated = function() {
+  this.lastUpdated = Date.now();
+  console.log("Matrix - set last updated")
+  console.log(this.lastUpdated);
+
+  return this.lastUpdated;
+};
 
 const Matrix = mongoose.model("Matrix", MatrixSchema);
 
