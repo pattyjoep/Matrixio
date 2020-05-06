6f8e0ac5cc80305c09dae2550fe2333630edad73
Patrick Dunn <pmdunn78@gmail.com>
2020-05-05 21:43:53 -0400

    Revert "removing value from email, woops"
    This reverts commit 7a07b4b09e72ed8a19d381ff630766d13dcdb60c.

--- client/src/pages/Settings/index.js
+++ client/src/pages/Settings/index.js
@@ -121,10 +121,11 @@ function Settings() {
                 className="inputName"
                 type="text"
                 name="email"
+                value={TeacherData.lastName}
                 onChange={e => setUpdateEmail(e.target.value)}
               />
               <button type="submit" className="updateTeacher-Btn">
-                Save Updates <i className="fas fa-pen-alt"></i>
+                Save Updates <i class="fas fa-pen-alt"></i>
               </button>
             </form>
           </Col>
