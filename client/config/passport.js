e90501a0b0ac6532a158d7a5468b0be61bca3d00
Matt Dambra <mpdambra@gmail.com>
2020-05-06 13:50:05 -0400

    adding logout strategy

--- client/config/passport.js
+++ client/config/passport.js
@@ -1,44 +1,67 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const db = require("../models");
+const db = require("../models/Teacher");
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
-passport.use(
-  new Strategy(
-    // Our user will sign in using an email, rather than a "username"
-    { usernameField: "email" },
-    function(email, password, done) {
-      // When a user tries to sign in this code runs
-      db.Teacher.findOne({ email: email })
-      .then(function(dbUser) {
-        // If there's no user with the given email
-        if (!dbUser) {
-          return done(null, false, {
-            message: "Incorrect email."
-          });
+/**
+ * Passport JS is used to manage and authentication user login status.
+ * Login strategy: Logs a user in by quering database to ensure user exists and passwords match.
+ * Logout strategy: Similiar to login, logout logs a user out and terminates their authenticated status.
+ */
+const loginStrategy = passport.use(
+    new Strategy(
+        // Our user will sign in using an email, rather than a "username"
+        { usernameField: "email" },
+        function (email, password, done) {
+            // When a user tries to sign in this code runs
+            db.Teacher.findOne({ email: email })
+                .then(function (dbUser) {
+                    // If there's no user with the given email
+                    if (!dbUser) {
+                        return done(null, false, {
+                            message: "Incorrect email."
+                        });
+                    }
+                    // If there is a user with the given email, but the password the user gives us is incorrect
+                    else if (!dbUser.validPassword(password)) {
+                        return done(null, false, {
+                            message: "Incorrect password."
+                        });
+                    }
+                    // If none of the above, return the user
+                    return done(null, dbUser);
+                });
         }
-        // If there is a user with the given email, but the password the user gives us is incorrect
-        else if (!dbUser.validPassword(password)) {
-          return done(null, false, {
-            message: "Incorrect password."
-          });
-        }
-        // If none of the above, return the user
-        return done(null, dbUser);
-      });
-    }
-  )
+    )
 );
 
+const logoutStrategy = new Strategy(
+    { usernameField: "email" },
+    function (
+        email,
+        password,
+        done
+    ) {
+        User.findOne({ email })
+            .lean()
+            .exec((err, user) => {
+                if (err) {
+                    return done(err, null);
+                }
+                if (!user) {
+                    return done("No user found ", null);
+                }
+                return done(null, user);
+            });
+    });
 // In order to help keep authentication state across HTTP requests,
 // Sequelize needs to serialize and deserialize the user
 // Just consider this part boilerplate needed to make it all work
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
+passport.serializeUser(function (user, cb) {
+    cb(null, user);
 });
 
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
+passport.deserializeUser(function (obj, cb) {
+    cb(null, obj);
 });
 
 // Exporting our configured passport
