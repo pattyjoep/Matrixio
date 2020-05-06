fa0fa5c0051d90985741600d7b24b355c00013e4
Matt Dambra <mpdambra@gmail.com>
2020-05-06 14:26:35 -0400

    Fixing passport error

--- client/config/passport/strategies/strategies.js
+++ client/config/passport/strategies/strategies.js
@@ -1,6 +1,6 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-const Teacher = require("../../models/Teacher");
+const Teacher = require("../../../../models/Teacher");
 const bcrypt = require("bcryptjs");
 
 /**
