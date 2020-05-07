b3c46ba14097d67120e4cac7b815a73ca753d343
Trick <petropatrickj@gmail.com>
2020-05-06 20:15:33 -0400

    updating new Matrix scripts

---
+++ client/src/components/NewMatrix/index.js
@@ -0,0 +1,31 @@
+import React from "react";
+import "./style.css";
+
+export default function newMatrix(props) {
+    return (
+        <table>
+            <tbody>
+                <tr>
+                    <th></th>
+                    {props.columnsArray.map(column => 
+                        <th contenteditable="true" placeholder="Enter Here"></th>
+                    )}
+                </tr>
+                {props.rowsArray.map(row => {
+                    return (
+                        <tr>
+                            <th contenteditable="true" placeholder="Enter Here"></th>
+                            {props.columnsArray.map(column => 
+                                <td>
+                                    <div className="status-card">
+                                        <div className="status-card-front" onClick={props.changeStatus}>X</div>
+                                    </div>
+                                </td>
+                            )}
+                        </tr>
+                    )
+                })}
+            </tbody>
+        </table> 
+    )
+}
